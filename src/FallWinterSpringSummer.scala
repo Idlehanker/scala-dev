17ce162f8027046dfe2a26be232df255d53bf128
idlehanker <hzujun@hotmail.com>
2018-04-01 10:29:31 +0800

    mv

--- src/FallWinterSpringSummer.scala
+++
@@ -1,8 +0,0 @@
-import SingletonAccumulator.calculate
-
-object FallWinterSpringSummer extends App {
-
-  for (season <- List("fall", "winter", "spring"))
-    println(season + ": " + calculate(season))
-
-}
